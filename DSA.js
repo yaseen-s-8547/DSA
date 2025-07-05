2eeb2417b3ff509e2f0abd53aa4f9659089a26c9
yaseen s <ycs613678@gmail.com>
2025-07-05 22:42:42 +0530

    initialcommit

--- DSA.js
+++ DSA.js
@@ -7,4 +7,13 @@ original=num;
     num=Math.floor(num/10);
  }
 console.log(original===reversed?"its a paliandrom":"not a paliandrom");
- 
+console.log("--------------------------------------------------------------------------");
+//w a p to print factorial
+num=5
+fact=1;
+i=1
+while(i<=num){
+   fact=fact*i
+   i++
+}
+ console.log("factorial of number is:"+fact);
